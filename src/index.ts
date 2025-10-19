003967176aa0cc519a1717e15c43c4eb25bdf6dc
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2025-10-18 23:25:50 -0700

    Added telemetry routes to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import telemetry from '#routes/telemetry.js';
 import trips from '#routes/trips.js';
 import 'dotenv/config';
 import express from 'express';
@@ -8,6 +9,7 @@ const port = process.env.PORT ?? '3000';
 app.use(express.json());
 
 app.use('/api/trips', trips);
+app.use('/api/telemetry', telemetry);
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);
